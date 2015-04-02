9c4d5b6599b136bb6860a6a6de560e89fd5962e2
zab88 <alexey.zabaykin@gmail.com>
2015-04-02 07:49:53 +0600

    custom improvements for cs.oball.ru

--- application/views/header.php
+++ application/views/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" context="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-    <title>CodeIgniter Bootstrap</title>
+    <title>CS</title>
     <meta name="description" content="">
 
     <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
--- application/views/home.php
+++ application/views/home.php
@@ -5,11 +5,17 @@ $urls = json_decode($site['urls']);
     <h1><?php echo $site['name'] ?></h1>
 <!--    <div class="well">--><?php //echo $site['description'] ?><!--</div>-->
     <pre><?php echo $site['description'] ?></pre>
-    <?php foreach ($urls as $url){
-        $splitted = explode(" ", $url);
-        $url_href = array_shift($splitted);
-        $url_name = implode(" ", $splitted);
-    ?>
-        <h3><a href="<?php echo $url_href; ?>" target="_blank"><?php echo $url_name; ?></a></h3>
+<!--    --><?php //foreach ($urls as $url){
+//        $splitted = explode(" ", $url);
+//        $url_href = array_shift($splitted);
+//        $url_name = implode(" ", $splitted);
+//    ?>
+<!--        <h3><a href="--><?php //echo $url_href; ?><!--" target="_blank">--><?php //echo $url_name; ?><!--</a></h3>-->
+<!---->
+<!--    --><?php //} ?>
+
+    <?php foreach ($urls as $url){ ?>
+    <iframe src="<?php echo $url ?>?view=iframe" width="100%" height="2000" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
     <?php } ?>
+
 </div>
